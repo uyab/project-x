7e745ceb8574c088be5ce56666aa9b09efd51d45
Bayu Hendra Winata <uyab.exe@gmail.com>
2023-11-28 16:18:06 +0700

    fix(training): validate fee and reward input type

--- modules/Training/resources/views/_form.blade.php
+++ modules/Training/resources/views/_form.blade.php
@@ -2,9 +2,9 @@
 	{!! form()->text('title')->label('Title') !!}
 	{!! form()->textarea('description')->label('Description') !!}
     {!! form()->selectMultiple('tags[]', $tags)->select($selectedTags)->label('Tags') !!}
-	{!! form()->text('fee')->label('Fee') !!}
-	{!! form()->text('reward')->label('Reward') !!}
-	{!! form()->datetimepicker('start_at')->label('Start_At') !!}
+	{!! form()->number('fee')->label('Fee') !!}
+	{!! form()->number('reward')->label('Reward') !!}
+	{!! form()->datetimepicker('start_at')->label('Start At') !!}
 {!! form()->action([
     form()->submit('Simpan'),
     form()->link('Batal', route('modules::training.index'))
